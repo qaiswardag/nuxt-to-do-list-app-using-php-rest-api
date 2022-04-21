e02fb440567ae9eaedf8e3153af3ff1fec46ddc5
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2022-04-21 16:10:24 +0200

    Update Project

--- pages/index.vue
+++ pages/index.vue
@@ -10,21 +10,18 @@
       <div v-if="!isPending">
         <div
             class="sm:grid sm:grid-cols-2 sm:gap-6">
-          <div v-for="(task, taskIdx) in tasks.tasks" :key="task.id" class="bg-white grid-cols-1">
-            <NuxtLink style="min-height: 350px" :to="'/task/' + task.id"
-                      class="w-full inline-block  group focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-300 rounded-md py-16 px-8">
-              <TaskDetails :task="task"></TaskDetails>
-            </NuxtLink>
+          <div v-for="(task, taskIdx) in tasks" :key="task.id"
+               class="grid-cols-1 border border-gray-50 p-4 rounded-md bg-yellow-50">
 
-
-            <Switch v-model="enabled"
-                    @click="toggleCompleted(task.id, task.completed)"
-                    :class="[enabled ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
-              <span class="sr-only">Use setting</span>
-              <span
-                  :class="[enabled ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']">
+            <div class="flex justify-center items-center gap-2 pb-4">
+              <Switch v-model="task.completed"
+                      @click="toggleCompleted(task.id, task.completed)"
+                      :class="[task.completed ? 'bg-yellow-400' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-300']">
+                <span class="sr-only">Use setting</span>
+                <span
+                    :class="[task.completed ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']">
       <span
-          :class="[enabled ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity']"
+          :class="[task.completed ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity']"
           aria-hidden="true">
         <svg class="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
           <path d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -32,7 +29,7 @@
         </svg>
       </span>
       <span
-          :class="[enabled ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity']"
+          :class="[task.completed ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity']"
           aria-hidden="true">
         <svg class="h-3 w-3 text-indigo-600" fill="currentColor" viewBox="0 0 12 12">
           <path
@@ -40,62 +37,66 @@
         </svg>
       </span>
     </span>
-            </Switch>
+              </Switch>
+              <button @click="deleteTask(task.id)"
+                      type="button"
+                      class="inline-flex items-center p-0.5 border border-transparent rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600">
+                <TrashIcon class="h-4 w-4" aria-hidden="true"/>
+              </button>
+            </div>
 
 
-            <fieldset class="py-4 bg-yellow-200 flex justify-start align-center pl-4">
-              <legend class="sr-only cursor-pointer">Checksbox variants</legend>
-              <div class="flex items-center justify-start">
-                <input
-                    @change="toggleCompleted(task.id, task.completed)"
-                    :id="'checkbox-' + task.id" type="checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label :id="'checkbox-' + task.id"
-                       class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  {{ task.completed === 'Y' ? 'Completed' : 'Not completed' }}
-                </label>
-              </div>
-            </fieldset>
+            <NuxtLink style="min-height: 350px" :to="'/task/' + task.id"
+                      class="w-full inline-block  group focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-300 rounded-md py-16 px-8 bg-white">
+              <TaskDetails :task="task"></TaskDetails>
+            </NuxtLink>
           </div>
+
+
         </div>
-
-        <nav aria-label="Pagination"
-             class="bg-white px-4 py-3 flex items-center justify-between sm:px-6 mt-4 rounded-md">
-          <div class="hidden sm:block">
-            <p class="text-gray-800">
-              Showing
-              {{ ' ' }}
-              <span
-                  class="font-medium">{{ (tasks.rows_returned * currentPage) - tasks.rows_returned + 1 }}</span>
-              {{ ' ' }}
-              to
-              {{ ' ' }}
-              <span class="font-medium">{{ tasks.rows_returned * currentPage }}</span>
-              {{ ' ' }}
-              of
-              {{ ' ' }}
-              <span class="font-medium">{{ tasks.total_rows }}</span>
-              {{ ' ' }}
-              results
-            </p>
-          </div>
-          <div class="flex-1 flex justify-between sm:justify-end gap-2">
-            <button :class="{'opacity-20 cursor-not-allowed': !tasks.has_previous_page}"
-                    class="relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md text-gray-700 bg-yellow-200 hover:bg-yellow-300"
-                    href="#"
-                    @click="loadTasks(currentPage = currentPage - 1)">
-              Previous
-            </button>
-
-            <button :class="{'opacity-20 cursor-not-allowed': !tasks.has_next_page}"
-                    class="relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md text-gray-700 bg-yellow-200 hover:bg-yellow-300"
-                    href="#"
-                    @click="loadTasks(currentPage = currentPage + 1)">
-              Next
-            </button>
-          </div>
-        </nav>
       </div>
+    </div>
+
+
+    <div v-if="tasksData">
+      <nav aria-label="Pagination"
+           class="bg-white px-4 py-3 flex items-center justify-between sm:px-6 mt-4 rounded-md">
+        <div class="hidden sm:block">
+          <p class="text-gray-800">
+            Showing
+            {{ ' ' }}
+            <span
+                class="font-medium">{{
+                (tasksData.data.rows_returned * currentPage) - tasksData.data.rows_returned + 1
+              }}</span>
+            {{ ' ' }}
+            to
+            {{ ' ' }}
+            <span class="font-medium">{{ tasksData.data.rows_returned * currentPage }}</span>
+            {{ ' ' }}
+            of
+            {{ ' ' }}
+            <span class="font-medium">{{ tasksData.data.total_rows }}</span>
+            {{ ' ' }}
+            results
+          </p>
+        </div>
+        <div class="flex-1 flex justify-between sm:justify-end gap-2">
+          <button :class="{'opacity-20 cursor-not-allowed': !tasksData.data.has_previous_page}"
+                  class="relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md text-gray-700 bg-yellow-200 hover:bg-yellow-300"
+                  href="#"
+                  @click="loadTasks(currentPage = currentPage - 1)">
+            Previous
+          </button>
+
+          <button :class="{'opacity-20 cursor-not-allowed': !tasksData.data.has_next_page}"
+                  class="relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md text-gray-700 bg-yellow-200 hover:bg-yellow-300"
+                  href="#"
+                  @click="loadTasks(currentPage = currentPage + 1)">
+            Next
+          </button>
+        </div>
+      </nav>
     </div>
 
     <DynamicModal
@@ -121,6 +122,7 @@
 <script setup>
 import {onMounted} from 'vue'
 import {Switch} from '@headlessui/vue'
+import {TrashIcon} from '@heroicons/vue/solid'
 
 
 const {
@@ -142,23 +144,37 @@ const secondModalButtonFunction = ref(null);
 const thirdModalButtonFunction = ref(null);
 
 // tasks
-const tasks = ref(null)
+const tasksData = ref()
+const tasks = ref([])
 // completed
 // import ajax
 const {isPending, error, loadData} = useAjax();
 // current page
 const currentPage = ref(1);
 //
-const enabled = ref(false)
 //
 //
 // async function
 const loadTasks = async function (pageNumber) {
+  const taskObj = ref({});
   try {
     // try
     const data = await loadData(`http://localhost/v1/tasks/page/${pageNumber}`, {}, 200)
+    tasksData.value = data;
+    //
+    data.data.tasks.forEach((task) => {
+      //
+      taskObj.value = {
+        id: task.id,
+        title: task.title,
+        description: task.description,
+        deadline: task.deadline,
+        completed: task.completed === 'Y' ? true : false,
+      }
 
-    tasks.value = await data.data;
+      // update tasks
+      tasks.value.push(taskObj.value);
+    })
     //
     // catch
   } catch (err) {
@@ -177,6 +193,8 @@ const loadTasks = async function (pageNumber) {
         thirdButtonText: 'Refresh Page',
       }
     });
+
+
     console.log('unable to fetch:', err);
   }
 }
@@ -184,24 +202,15 @@ const loadTasks = async function (pageNumber) {
 //
 //
 const toggleCompleted = async function (id, completedTask) {
-
-  let completed = null;
-
-  if (completedTask === 'Y') {
-    completed = 'N'
-  }
-
-  if (completedTask === 'N') {
-    completed = 'Y'
-  }
+  // task object
+  const taskObj = ref({});
   //
-
   // update task completed
   try {
     // try
-    const taskData = await loadData(`http://localhost/v1/tasks/${id}`, {
+    const data = await loadData(`http://localhost/v1/tasks/${id}`, {
       method: 'PATCH',
-      body: JSON.stringify({completed: completed}),
+      body: JSON.stringify({completed: completedTask === true ? 'Y' : 'N'}),
       headers: {
         'Content-Type': 'application/json'
       },
@@ -209,32 +218,33 @@ const toggleCompleted = async function (id, completedTask) {
 
 
     // handle error in returned data
-    if (taskData.statusCode === 400) {
+    if (data.statusCode === 400) {
 
       let messageError = [];
 
       // foreach on message
-      taskData.messages.forEach(message => {
+      data.messages.forEach(message => {
         messageError.push(message);
       })
+
 
       throw new Error(messageError.toString());
     }
 
-    // console.log('ålå: ', await taskData.data.tasks[0].id)
 
-
-    tasks.value.tasks = await taskData.data.tasks.filter(task => {
-      if (task.id === taskData.data.tasks[0].id) {
-        return taskData.data.tasks[0];
+    data.data.tasks.forEach((task) => {
+      //
+      taskObj.value = {
+        id: task.id,
+        title: task.title,
+        description: task.description,
+        deadline: task.deadline,
+        completed: task.completed === 'Y' ? true : false,
       }
-      return tasks.value.tasks
+
+      // update tasks
+      tasks.value.push(taskObj.value);
     })
-
-    console.log('hørrr::: ', tasks.value)
-
-    // update task
-    // tasks.value = await taskData.data;
 
 
     // catch
@@ -271,6 +281,67 @@ const toggleCompleted = async function (id, completedTask) {
 
 //
 //
+const deleteTask = async function (id) {
+
+  console.log('task id is: ', id)
+
+  try {
+
+    // const response = await loadData(`http://localhost/v1/tasks/${id}`, {
+    //
+    //   method: 'DELETE',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    // }, 200,)
+    //
+    // console.log('delete response is: ', response);
+    //
+
+    dynamicModal({
+      options: {
+        optionsAmount: 2,
+      },
+      design: {
+        typeOfModal: 'success',
+        gridColumnAmount: 1,
+      },
+      content: {
+        title: 'Task have been deleted',
+        firstButtonText: 'Close',
+      }
+    });
+
+    // update tasks
+    let min = tasks.value.filter(task => {
+      console.log('the task is:', task)
+      return task.id === id
+    })
+    console.log('min: ', min)
+
+    // // handle modal click
+    firstModalButtonFunction.value = function () {
+      error.value = false;
+      openModal.value = false;
+    }
+  } catch (err) {
+    dynamicModal({
+      options: {
+        optionsAmount: 2,
+      },
+      design: {
+        typeOfModal: 'error',
+        gridColumnAmount: 2,
+      },
+      content: {
+        title: err.message,
+        firstButtonText: 'Close',
+        thirdButtonText: 'Refresh Page',
+      }
+    });
+    console.log('unable to fetch:', err);
+  }
+}
 //
 //
 //
